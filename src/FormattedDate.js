a5557c52c486e36ee9b32a65b096875703715e47
Gulmira-K <73161951+Gulmira-K@users.noreply.github.com>
2021-01-10 14:00:00 +0100

    Added css component

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./FormattedDate.css";
 
 export default function FormattedDate(props) {
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -19,11 +20,11 @@ export default function FormattedDate(props) {
     let day = days[props.date.getDay()];
     let date = props.date.getDate();
     let month = months[props.date.getMonth()];
-    let hour = date.getHours();
+    let hour = props.date.getHours();
         if (hour < 10) {
          hour = `0${hour}`;
      }
-    let minuts = date.getMinutes();
+    let minuts = props.date.getMinutes();
         if (minuts < 10) {
         minuts = `0${minuts}`;
     }
